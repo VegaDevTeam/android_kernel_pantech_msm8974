7f5a1ab02ee7d4ab384a5a466e559bf471b9c29a
zhaochengw <zhaochengw@163.com>
2014-06-12 10:34:43 +0800

    import A890K v2.09

---
+++ include/pantech/CUST_PANTECH_IMS.h
@@ -0,0 +1,10 @@
+#ifndef __CUST_PANTECH_IMS_H__
+#define __CUST_PANTECH_IMS_H__
+
+
+// Define this feature to turn on the IMS PPS Loging function
+#define FEATURE_PAN_IMS_PPS_LOGGING
+
+
+#endif /*__CUST_PANTECH_IMS_H__*/
+
